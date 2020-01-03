b76835aadc5e3c105abbe1dfaff39a6c204fcdc4
Alley M. Carvalho <alleycarvalho@gmail.com>
2020-01-03 16:10:14 -0300

    Private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
+
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" exact component={SignIn} />
+      <Route path="/login" component={SignIn} />
+
+      <Route path="/" exact component={Dashboard} isPrivate />
+
+      <Route path="/" component={() => <h1>404 - Not Found</h1>} />
     </Switch>
   );
 }
