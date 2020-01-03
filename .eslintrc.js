3fe7def19c3233a4dec28e214b5c0a657cbe0ade
Alley M. Carvalho <alleycarvalho@gmail.com>
2020-01-03 08:25:37 -0300

    ESLint, Prettier & EditorConfig

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'prettier',
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    camelcase: 'off',
+    'global-require': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-one-expression-per-line': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-native/no-raw-text': 'off',
+  },
+};
